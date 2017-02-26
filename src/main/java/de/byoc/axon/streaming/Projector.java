f8e757ee13f5d65573156494321373c1a3dec5e7
pepperbob <github@byoc.de>
2017-02-26 07:39:21 +0100

    Fix: Use Axon utilities to stream events

--- src/main/java/de/byoc/axon/streaming/Projector.java
+++ src/main/java/de/byoc/axon/streaming/Projector.java
@@ -1,87 +1,31 @@
 package de.byoc.axon.streaming;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
 
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.SimpleEventHandlerInvoker;
 import org.axonframework.messaging.Message;
 import org.axonframework.messaging.MessageStream;
-import org.axonframework.messaging.MetaData;
 import org.axonframework.messaging.StreamableMessageSource;
 
 public final class Projector {
 
   private final StreamableMessageSource<? extends Message<?>> source;
-
+  
   public Projector(StreamableMessageSource<? extends Message<?>> source) {
     this.source = source;
   }
 
   public void project(Object handler) {
-    final ProjectionMethods methods = new ProjectionMethods(handler.getClass().getMethods());
-
+    final SimpleEventHandlerInvoker invoker = new SimpleEventHandlerInvoker(handler);
     try (final MessageStream<? extends Message<?>> stream = this.source.openStream(null)) {
       while (stream.hasNextAvailable()) {
         final Message<?> next = stream.nextAvailable();
-
-        methods.filter(next.getPayloadType())
-                .forEach(method -> method.invoke(handler, next));
+        if (next instanceof EventMessage) {
+          invoker.handle((EventMessage<?>) next);
+        }
       }
-    } catch (InterruptedException ex) {
+    } catch (Exception ex) {
       throw new RuntimeException(ex);
     }
   }
-
-  private class ProjectionMethods {
-
-    private Map<Class<?>, MethodInvoker> map = new HashMap<>();
-
-    public ProjectionMethods(Method[] methods) {
-      for (Method method : methods) {
-        final boolean meta;
-        if (method.getParameterCount() == 1) {
-          meta = false;
-        } else if (method.getParameterCount() == 2
-                && method.getParameterTypes()[1].isAssignableFrom(MetaData.class)) {
-          meta = true;
-        } else {
-          continue;
-        }
-        
-        map.put(method.getParameterTypes()[0], new MethodInvoker(method, meta));
-      }
-    }
-
-    public Stream<MethodInvoker> filter(Class<?> type) {
-      return map.keySet().stream()
-              .filter(clazz -> clazz.isAssignableFrom(type))
-              .map(map::get);
-    }
-  }
-
-  private class MethodInvoker {
-
-    private final Method method;
-    private final boolean meta;
-
-    private MethodInvoker(Method method, boolean meta) {
-      this.method = method;
-      this.meta = meta;
-    }
-
-    public void invoke(Object target, Message<?> message) {
-      try {
-        if (meta) {
-          method.invoke(target, message.getPayload(), message.getMetaData());
-        } else {
-          method.invoke(target, message.getPayload());
-        }
-      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-        throw new RuntimeException(ex.getCause() != null ? ex.getCause() : ex);
-      }
-    }
-  }
-
 }
--- src/main/java/de/byoc/axon/streaming/handler/TestReadModel.java
+++ src/main/java/de/byoc/axon/streaming/handler/TestReadModel.java
@@ -3,6 +3,7 @@ package de.byoc.axon.streaming.handler;
 import de.byoc.axon.streaming.events.TableSeated;
 import de.byoc.eventing.events.FoodOrdered;
 import de.byoc.eventing.events.InvoicePaid;
+import org.axonframework.eventhandling.EventHandler;
 
 public class TestReadModel {
 
@@ -13,21 +14,25 @@ public class TestReadModel {
   public int totalPersons;
   public int salesPerPerson;
 
+  @EventHandler
   public void falls(FoodOrdered event) {
     this.numberItemsOrdered++;
     this.totalQuantity += event.qty;
   }
 
+  @EventHandler
   public void falls(InvoicePaid event) {
     this.invoiceTotal = event.amount;
     this.avgInvoiceItem = this.invoiceTotal / this.totalQuantity;
   }
   
+  @EventHandler
   public void falls(TableSeated event) {
     this.totalPersons += event.persons;
     this.salesPerPerson = totalPersons == 0 ? 0 : invoiceTotal/totalPersons;
   }
   
+  @EventHandler
   private void abc(TableSeated event) {
     this.totalPersons += event.persons;
     this.salesPerPerson = totalPersons == 0 ? 0 : invoiceTotal/totalPersons;
